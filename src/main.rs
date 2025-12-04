b4d2c6b5aed443c12d36d1d18c61259a8bb5d4cd
amit0365 <47255508+amit0365@users.noreply.github.com>
2025-12-03 22:42:31 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -160,13 +160,12 @@ async fn main() {
                     match master_stream.read(&mut buf).await {
                         Ok(0) => (),
                         Ok(n) => {
-                            println!("n {}", n);
                             let buffer_str = from_utf8(&buf[..n]).unwrap().split("\r\n").collect::<Vec<&str>>();;
                             match buffer_str[0]{
                                 "+PONG" => {
-                                    let replconf_msg1 = encode_resp_array(&vec![format!("REPLCONF listening-port {}", port)]);
+                                    let replconf_msg1 = encode_resp_array(&vec!["REPLCONF".to_string(), "listening-port".to_string(), port]);
                                     master_stream.write(replconf_msg1.as_bytes()).await.unwrap();
-                                    let replconf_msg2 = encode_resp_array(&vec!["REPLCONF capa psync2".to_string()]);
+                                    let replconf_msg2 = encode_resp_array(&vec!["REPLCONF".to_string(), "capa".to_string(), "psync2".to_string()]);
                                     master_stream.write(replconf_msg2.as_bytes()).await.unwrap();
                                 },
 
